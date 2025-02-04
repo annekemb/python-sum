ca97201b6d5898da2280b24504f7866e8b872f2a
MarkMelotto <markmelotto@gmail.com>
2025-02-04 11:56:56 +0100

    added a new substract_three_numbers.py with the function to substract 3 numbers

---
+++ substract_three_numbers.py
@@ -0,0 +1,13 @@
+def substract_two_numbers(a,b,c):
+    """
+    Subtracts three numbers and returns the result.
+
+    Parameters:
+    a (int): the first number
+    b (int): the second number
+    c (int): the third number
+
+    Return:
+    int: the subtraction of the three numbers
+    """
+    return a - b - c
