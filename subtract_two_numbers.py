7c73859729e32b9d385c3dc83ea7d4e0ae8b0747
Anneke Batenburg <annekebatenburg@gmail.com>
2025-02-04 11:29:00 +0100

    subtraction function

---
+++ subtract_two_numbers.py
@@ -0,0 +1,12 @@
+def sum_two_numbers(a,b);
+    """
+    Subtracts two numbers and returns the result.
+
+    Parameters:
+    a (int): the first number
+    b (int): the second number
+
+    Return:
+    int: the sum of the two numbers
+    """
+    return a - b
